d183b9f2349cb2cff6e52ea62c9c829dfa7b2ad2
JorgeArandaP <aranda.p.jorge@gmail.com>
2024-07-07 22:52:29 +0800

    Se agrego boton para cambiar el estado de tarea realizada

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,24 +12,29 @@ function renderTarea() {
         <tr>
             <th scope="row">${tarea.id}</th>
             <td>${tarea.descrip}</td>
-            <th><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></th>
+            <th><input onclick="cambiar(this, ${tarea.id})" class="form-check-input" type="checkbox" value="" id="defaultCheck1"></th>
             <th><button onclick="borrar(${tarea.id})" type="button" class="btn btn-outline-danger">Borrar</button></th>
         </tr>`;
     }
     listaDeTareas.innerHTML = html;
+
     let html1 = `
     <p>Total de tareas: ${tareas.length}</p>
-    <p>Realizadas: </p>`;
+    <p>Realizadas: ${tareas.filter(tarea => tarea.realizada == true).length}</p>`;
     resTareas.innerHTML = html1;
 } 
+
+function renderResumen() {
+    let html1 = `
+    <p>Total de tareas: ${tareas.length}</p>
+    <p>Realizadas: ${tareas.filter(tarea => tarea.realizada == true).length}</p>`;
+    resTareas.innerHTML = html1;
+}
 
 btnAgregar.addEventListener('click', () => {
     const nuevaTarea = tareaInput.value
     tareas.push({id: Date.now(), descrip: nuevaTarea, realizada: false})
-    tareaInput.value = '';
-
-    
-
+    tareaInput.value = '';  
     renderTarea()
 
 })
@@ -40,5 +45,12 @@ function borrar(id){
     renderTarea()
 }
 
+function cambiar(checkbox, id) {
+    const esClickeado = checkbox.checked;
+    const index = tareas.findIndex((ele) => ele.id == id)
+    tareas[index].realizada = esClickeado ? true : false;
+   
+    renderResumen()
+}
 
 
