4fa057d5d5f396172e94d3d4537b083f39f0c937
JorgeArandaP <aranda.p.jorge@gmail.com>
2024-07-07 21:42:40 +0800

    Se agrego el boton para borrar tareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,7 @@ const tareaInput = document.querySelector('#nuevaTarea')
 const btnAgregar = document.querySelector('#agregarTarea')
 const resTareas = document.querySelector('#resumenTareas')
 
-btnAgregar.addEventListener('click', () => {
-    const nuevaTarea = tareaInput.value
-    tareas.push({id: Date.now(), descrip: nuevaTarea, realizada: false})
-    tareaInput.value = ''
-
-    html1 = `
-    <p>Total de tareas: ${tareas.length}</p>
-    <p>Realizadas: </p>`;
-    resTareas.innerHTML = html1;
+function renderTarea() {
 
     let html = ''
     for (let tarea of tareas) {
@@ -20,12 +12,33 @@ btnAgregar.addEventListener('click', () => {
         <tr>
             <th scope="row">${tarea.id}</th>
             <td>${tarea.descrip}</td>
+            <th><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></th>
+            <th><button onclick="borrar(${tarea.id})" type="button" class="btn btn-outline-danger">Borrar</button></th>
         </tr>`;
-        listaDeTareas.innerHTML = html;
-
     }
+    listaDeTareas.innerHTML = html;
+    let html1 = `
+    <p>Total de tareas: ${tareas.length}</p>
+    <p>Realizadas: </p>`;
+    resTareas.innerHTML = html1;
+} 
+
+btnAgregar.addEventListener('click', () => {
+    const nuevaTarea = tareaInput.value
+    tareas.push({id: Date.now(), descrip: nuevaTarea, realizada: false})
+    tareaInput.value = '';
+
+    
+
+    renderTarea()
 
 })
+
+function borrar(id){
+    const index = tareas.findIndex((ele) => ele.id == id)
+    tareas.splice(index, 1)
+    renderTarea()
+}
 
 
 
